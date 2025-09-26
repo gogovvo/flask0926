4a7ff483090918c20722568931f088f39abf15d3
EUNSE <96850872+gogovvo@users.noreply.github.com>
2025-09-26 20:37:20 +0900

    db connect test

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+import os
+from flask import Flask, jsonify
+from sqlalchemy import create_engine, text
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return render_template('./index.html')
+
+DB_URI = os.getenv("DATABASE_URL")
+engine = create_engine(DB_URI, pool_pre_ping=True)
+
+@app.get("/")
+def health():
+    # DB 연결 테스트
+    with engine.connect() as conn:
+        v = conn.execute(text("SELECT 1")).scalar()
+    return jsonify(ok=True, db=v)
